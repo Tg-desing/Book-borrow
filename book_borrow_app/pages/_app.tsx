7246d3420584acb9e738b51be488ec4c09b4ad0b
Taegon <taegon@cusee.net>
2023-11-16 02:08:35 +0900

    first update

---
+++ book_borrow_app/pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
+}
+
+export default MyApp;
